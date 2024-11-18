8342f9bb10ad05fe7d6e50b0f6d95dcd591ef342
asanromans <asanromans01@iescastelar.com>
2024-11-18 09:50:34 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,10 @@
+@echo off
+::Crear diferentes directorios
+::Adrián San Román
+::13-11-2024
+md %userprofile%\asir %userprofile%\dam %userprofile%\smr %userprofile%\daw %userprofile%\ceti
+md %userprofile%\asir\docs %userprofile%\asir\pract %userprofile%\asir\exam
+md %userprofile%\dam\docs %userprofile%\dam\pract %userprofile%\dam\exam
+md %userprofile%\smr\docs %userprofile%\smr\pract %userprofile%\smr\exam
+md %userprofile%\daw\docs %userprofile%\daw\pract %userprofile%\daw\exam
+md %userprofile%\ceti\docs %userprofile%\ceti\pract %userprofile%\ceti\exam
